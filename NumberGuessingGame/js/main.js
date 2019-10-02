d7d5d0a2cc6a597e21ecdba51c198af946245955
Tarulah <taru.lahtinen@metropolia.fi>
2019-10-02 19:42:01 +0300

    Number guessing game added

---
+++ NumberGuessingGame/js/main.js
@@ -0,0 +1,24 @@
+﻿// Your code here!
+
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+
+//For debugging the number generator
+//document.getElementById("debug").innerHTML = randomNumber;
+
+document.getElementById("submit").onclick = 
+function checkGuess() {
+	let guess = document.getElementById("guess");
+
+	if (guess.value == randomNumber) {
+		document.getElementById("gameState").innerHTML = "Oikein!!";
+		randomNumber = Math.floor(Math.random() * 10) + 1;
+	}
+
+	else if (guess.value < randomNumber) {
+		document.getElementById("gameState").innerHTML = "Liian pieni.";
+	}
+
+	else {
+		document.getElementById("gameState").innerHTML = "Liian iso.";
+	}
+}
